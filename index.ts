05f4899b6c3ccc78ab6bf8fc90c966c85b1cf0db
SanthD <prasanthsatsangi@gmail.com>
2023-02-22 01:09:20 +0530

    new data added

--- index.ts
+++ index.ts
@@ -1,6 +1,6 @@
 import express, { Express, Request, Response } from 'express';
 import dotenv from 'dotenv';
-
+import config from './src/common/configuration/config';
 dotenv.config();
 
 const app: Express = express();
